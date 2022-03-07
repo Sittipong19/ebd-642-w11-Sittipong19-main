bdea3f93a46b442239bf9c39774fb83b676f9df6
Sittipong19 <75230330+Sittipong19@users.noreply.github.com>
2022-03-07 19:31:53 +0700

    Add files via upload

---
+++ ebd-642-w11-Sittipong19-main-main/ebd-642-w11-Sittipong19-main/ebd-642-w11-Sittipong19-main/Components/Content.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="pl-24">
+    <h1>Computer Science </h1>
+    <h1>Information Technology</h1>
+    <h1>Computer Network</h1>
+    <h1>Geographic Information</h1>
+</div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
